003dd1034290a323f67baf6fea7c02448fe642d9
Shikhrrr <136259398+Shikhrrr@users.noreply.github.com>
2023-08-16 14:24:04 +0530

    Add files via upload

---
+++ Web Projects/Quiz/Quiz.js
@@ -0,0 +1,12 @@
+const questions = [
+    {
+        questions: "Question number one-",
+        answers: [
+            { text: 'Option 1', correct: false },
+            { text: 'Option 2', correct: true },
+            { text: 'Option 3', correct: false },
+            { text: 'Option 4', correct: false },
+
+        ]
+    }
+]
